12d0c6ddb7d6048678ae67b6cf1e7ae81fba4232
ryanlo713 <12672523+ryanlo713@users.noreply.github.com>
2023-08-10 00:29:57 +0800

    search test

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,6 +3,7 @@ import { getDirname, path } from '@vuepress/utils'
 import { registerComponentsPlugin } from '@vuepress/plugin-register-components'
 import ReplaceParenthesesPlugin from './plugins/replaceParentheses'
 import theme from "./theme";
+import { docsearchPlugin } from "@vuepress/plugin-docsearch";
 
 const __dirname = getDirname(import.meta.url)
 
@@ -20,7 +21,55 @@ export default defineUserConfig({
     registerComponentsPlugin({
       componentsDir: path.resolve(__dirname, './components'),
     }),
-    ReplaceParenthesesPlugin()
+    ReplaceParenthesesPlugin(),
+    docsearchPlugin({
+      appId: "Q8IC2K1VXW",
+      apiKey: "f254e5608a1e396f9bde071de83c5284",
+      indexName: "yuorbio",
+      locales: {
+        "/": {
+          placeholder: "搜索文档",
+          translations: {
+            button: {
+              buttonText: "搜索文档",
+              buttonAriaLabel: "搜索文档",
+            },
+            modal: {
+              searchBox: {
+                resetButtonTitle: "清除查询条件",
+                resetButtonAriaLabel: "清除查询条件",
+                cancelButtonText: "取消",
+                cancelButtonAriaLabel: "取消",
+              },
+              startScreen: {
+                recentSearchesTitle: "搜索历史",
+                noRecentSearchesText: "没有搜索历史",
+                saveRecentSearchButtonTitle: "保存至搜索历史",
+                removeRecentSearchButtonTitle: "从搜索历史中移除",
+                favoriteSearchesTitle: "收藏",
+                removeFavoriteSearchButtonTitle: "从收藏中移除",
+              },
+              errorScreen: {
+                titleText: "无法获取结果",
+                helpText: "你可能需要检查你的网络连接",
+              },
+              footer: {
+                selectText: "选择",
+                navigateText: "切换",
+                closeText: "关闭",
+                searchByText: "搜索提供者",
+              },
+              noResultsScreen: {
+                noResultsText: "无法找到相关结果",
+                suggestedQueryText: "你可以尝试查询",
+                reportMissingResultsText: "你认为该查询应该有结果？",
+                reportMissingResultsLinkText: "点击反馈",
+              },
+            },
+          },
+        },
+      },
+    }),
   ],
   theme,
   // Enable it with pwa
