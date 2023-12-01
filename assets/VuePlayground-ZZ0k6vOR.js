4b33e499b319eeadf3f3c40eac856e3bba5a9ffb
ryanlo713 <12672523+ryanlo713@users.noreply.github.com>
2023-12-01 15:01:36 +0000

    Deploying to gh-pages from @ yuorb/yuorb.github.io@ae3c16114a2431e77cad0cc84e337f3926880efb 🚀

---
+++ assets/VuePlayground-ZZ0k6vOR.js
@@ -0,0 +1,7 @@
+import{h as _,i as m,j as f,s,k as R,v as g,l as y,m as a,$ as h,_ as u}from"./app-I9cW3Q06.js";const w=e=>JSON.parse(decodeURIComponent(e));var V=_({name:"VuePlayground",props:{title:{type:String,default:""},files:{type:String,required:!0},settings:{type:String,default:"{}"}},setup(e){const n=m(),i=f(!0),t=s(),l=s(),r=s(),o=R(()=>g({},n,w(e.settings))),d=async()=>{const[{ReplStore:v,Repl:p},{default:c}]=await Promise.all([u(()=>import("./vue-repl-dDRCMNgL.js"),__vite__mapDeps([0,1,2])),u(()=>import("./codemirror-editor-4XPrMO9h.js"),__vite__mapDeps([3,2,1]))]);t.value=p,r.value=c,l.value=new v({serializedState:decodeURIComponent(e.files)}),o.value.vueVersion&&await l.value.setVueVersion(o.value.vueVersion)};return y(async()=>{await d(),i.value=!1}),()=>[a("div",{class:"vue-playground-wrapper"},[e.title?a("div",{class:"header"},decodeURIComponent(e.title)):null,a("div",{class:"repl-container"},[i.value?a(h,{class:"preview-loading",height:192}):null,t.value?a(t.value,{editor:r.value,store:l.value,autoResize:!0,...o.value,layout:"horizontal"}):null])])]}});export{V as default};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/vue-repl-dDRCMNgL.js","assets/app-I9cW3Q06.js","assets/utils-5585d0be-XOQqDqQW.js","assets/codemirror-editor-4XPrMO9h.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
