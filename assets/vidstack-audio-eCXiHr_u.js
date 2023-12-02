7b890a6989ea878b0bbbab9ff3042d1d4388cb5e
ryanlo713 <12672523+ryanlo713@users.noreply.github.com>
2023-12-02 20:09:36 +0000

    Deploying to gh-pages from @ yuorb/yuorb.github.io@d63c44df77cc8f14dbec3fcbd320b13d41e4378a 🚀

--- assets/vidstack-audio-eCXiHr_u.js
+++
@@ -1,1 +0,0 @@
-import{H as t}from"./vidstack-2gN4S9Em-nPgwQfHm.js";import"./vidstack-1H0xyD-V-EtaFaH07.js";import"./app-I9cW3Q06.js";class u extends t{constructor(){super(...arguments),this.$$PROVIDER_TYPE="AUDIO"}get type(){return"audio"}setup(e){super.setup(e),this.type==="audio"&&e.delegate.a("provider-setup",this)}get audio(){return this.b}}export{u as AudioProvider};
