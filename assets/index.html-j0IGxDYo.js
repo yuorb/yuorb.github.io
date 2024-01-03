85f02bcc516ec126d1ad2bf8280903c4a84a2d2c
ryanlo713 <12672523+ryanlo713@users.noreply.github.com>
2024-01-03 07:32:52 +0000

    Deploying to gh-pages from @ yuorb/yuorb.github.io@d2506836fecd29328538719d2da94bba6aa5aee9 🚀

---
+++ assets/index.html-j0IGxDYo.js
@@ -0,0 +1,22 @@
+import{_ as i}from"./plugin-vue_export-helper-x3n3nnut.js";import{r as a,o as c,c as d,a as e,b as n,n as l,g as s,d as t,e as u}from"./app-C7xv3k3O.js";const h={},m=e("div",{style:{"text-align":"center"}},[e("h1",{id:"interpretes-et-programmatei-翻译及开发成员",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#interpretes-et-programmatei-翻译及开发成员","aria-hidden":"true"},"#"),t(" Interpretes et Programmatei 翻译及开发成员")])],-1),p={class:"vp-card-container"},_=e("hr",null,null,-1),g=e("div",{style:{"text-align":"center"}},[e("h1",{id:"yuorb",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#yuorb","aria-hidden":"true"},"#"),t(),e("em",null,"Yuorb")]),e("p",null,"该形式分析为 √-RB- 的词干零加上无形界，表示「含义、翻译、解释」；基本涵盖了本组织的目的。"),e("p",null,"本组织的全称是 Hlesaḑcä-Yuorba :Han:，即「以汉语释译」。"),e("p",null,"Malëuţřaitaẓa: 学习 New Ithkuil 的地方。"),e("p",null,"Malëuţřait 是该语言的内名，意思是「本语言交流系统」。")],-1),b=e("hr",null,null,-1),C={href:"http://creativecommons.org/licenses/by-nc-sa/4.0/",target:"_blank",rel:"noopener noreferrer"},v=e("br",null,null,-1),x={href:"http://creativecommons.org/licenses/by-nc-sa/4.0/",target:"_blank",rel:"noopener noreferrer"},y=e("img",{src:"https://licensebuttons.net/l/by-nc-sa/4.0/88x31.png",alt:"",loading:"lazy"},null,-1),f=e("img",{src:"https://img.shields.io/badge/License-CC BY--NC--SA 4.0-lightgrey.svg",alt:"",loading:"lazy"},null,-1),k={href:"http://creativecommons.org/licenses/by-nc-sa/4.0/",target:"_blank",rel:"noopener noreferrer"};function N(A,B){const r=a("VPCard"),o=a("ExternalLinkIcon");return c(),d("div",null,[m,e("div",p,[n(r,l(s({title:"天邪弱 Lucifer Caelius Delicatus",desc:"Hliošraumhâ-Ulalëučha hi Allwizyëilliöhla Aḷḷtḷiẓdövvei Altyaswë’illä hiu",logo:"https://avatars.githubusercontent.com/u/12672523?s=400",link:"https://github.com/ryanlo713",background:"rgba(127, 71, 221, 0.1)",color:"var(--my-bg)"})),null,16),n(r,l(s({title:"Lomírus",desc:"暂缺",logo:"https://avatars.githubusercontent.com/u/21173956?v=4",link:"https://github.com/lomirus",background:"rgba(127, 71, 221, 0.1)",color:"var(--my-bg)"})),null,16)]),_,g,b,e("p",null,[t("Shield: "),e("a",C,[t("CC BY-NC-SA 4.0"),n(o)])]),e("p",null,[t("This work is licensed under a"),v,e("a",x,[t("Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License"),n(o)]),t(".")]),e("p",null,[y,t(),f,t(),e("a",k,[t("CC BY-NC-SA 4.0"),n(o)])]),u(`
+<script>
+  function copyQQGroupCode() {
+    const qqGroupCode = '865538600';
+
+    // 创建临时 textarea 元素
+    const textarea = document.createElement('textarea');
+    textarea.value = qqGroupCode;
+    document.body.appendChild(textarea);
+
+    // 选择并复制文本
+    textarea.select();
+    document.execCommand('copy');
+
+    // 移除临时元素
+    document.body.removeChild(textarea);
+
+    // 弹出提示消息
+    alert('复制群号成功');
+  }
+<\/script>
+`)])}const I=i(h,[["render",N],["__file","index.html.vue"]]);export{I as default};
