5e9cce839e1142e86c552bb8a642d182c001c489
ryanlo713 <12672523+ryanlo713@users.noreply.github.com>
2024-01-31 03:36:59 +0800

    maintain

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,13 +1,14 @@
-import { defineUserConfig } from "vuepress";
+import { viteBundler } from '@vuepress/bundler-vite'
+import { defineUserConfig } from "vuepress"
 import { getDirname, path } from '@vuepress/utils'
 import { registerComponentsPlugin } from '@vuepress/plugin-register-components'
 import ReplaceParenthesesPlugin from './plugins/replaceParentheses'
 import theme from "./theme";
-import { docsearchPlugin } from "@vuepress/plugin-docsearch";
 
 const __dirname = getDirname(import.meta.url)
 
 export default defineUserConfig({
+  bundler: viteBundler(),
   base: "/",
   head: [['link', { rel: 'icon', href: '/logo.png' }]],
   locales: {
@@ -22,54 +23,6 @@ export default defineUserConfig({
       componentsDir: path.resolve(__dirname, './components'),
     }),
     ReplaceParenthesesPlugin(),
-    docsearchPlugin({
-      appId: "Q8IC2K1VXW",
-      apiKey: "f254e5608a1e396f9bde071de83c5284",
-      indexName: "yuorbio",
-      locales: {
-        "/": {
-          placeholder: "搜索文档",
-          translations: {
-            button: {
-              buttonText: "搜索文档",
-              buttonAriaLabel: "搜索文档",
-            },
-            modal: {
-              searchBox: {
-                resetButtonTitle: "清除查询条件",
-                resetButtonAriaLabel: "清除查询条件",
-                cancelButtonText: "取消",
-                cancelButtonAriaLabel: "取消",
-              },
-              startScreen: {
-                recentSearchesTitle: "搜索历史",
-                noRecentSearchesText: "没有搜索历史",
-                saveRecentSearchButtonTitle: "保存至搜索历史",
-                removeRecentSearchButtonTitle: "从搜索历史中移除",
-                favoriteSearchesTitle: "收藏",
-                removeFavoriteSearchButtonTitle: "从收藏中移除",
-              },
-              errorScreen: {
-                titleText: "无法获取结果",
-                helpText: "你可能需要检查你的网络连接",
-              },
-              footer: {
-                selectText: "选择",
-                navigateText: "切换",
-                closeText: "关闭",
-                searchByText: "搜索提供者",
-              },
-              noResultsScreen: {
-                noResultsText: "无法找到相关结果",
-                suggestedQueryText: "你可以尝试查询",
-                reportMissingResultsText: "你认为该查询应该有结果？",
-                reportMissingResultsLinkText: "点击反馈",
-              },
-            },
-          },
-        },
-      },
-    }),
   ],
   theme,
   // Enable it with pwa
