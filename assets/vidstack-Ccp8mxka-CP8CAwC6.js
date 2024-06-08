54884b318183eb4cb6d09fe93a2fb37295ac2aa8
lomirus <lomirus.dev@protonmail.com>
2024-06-08 10:35:33 +0000

    Deploying to gh-pages from @ yuorb/yuorb.github.io@a15da067cf01cc53fddbb104c6417637fd4abcb2 🚀

--- assets/vidstack-Ccp8mxka-CP8CAwC6.js
+++
@@ -1,1 +0,0 @@
-import{bq as i,br as t}from"./app-BmBkTn9W.js";class n{constructor(s){this.La=s}Xa(){i(this.ya)&&this.fg()}$(){t(this.ya)&&window.cancelAnimationFrame(this.ya),this.ya=void 0}fg(){this.ya=window.requestAnimationFrame(()=>{i(this.ya)||(this.La(),this.fg())})}}export{n as R};
